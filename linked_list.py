5f22ff9932e2381e092610409de70c0c290e4cab
Jonatas Fontele <jonatasfontele@gmail.com>
2021-05-01 15:43:22 -0300

    Exemplo de lista cadeada em Python

---
+++ linked_list.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+
+def main():
+    linked_list = LinkedList()
+    print(linked_list)
+    first_node = Node("a")
+    linked_list.head = first_node
+    print(linked_list)
+    second_node = Node("b")
+    third_node = Node("c")
+    first_node.next = second_node
+    second_node.next = third_node
+    print(linked_list)
+
+
+if __name__ == "__main__":
+    main()
